92537d12d7f7a6b59411ade88de82a1d2ad0f020
Majid.mhdp <Majid.mhdp@gmail.com>
2026-03-18 21:31:12 +0800

    users components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,20 +7,20 @@ import {
   Navigate,
 } from "react-router-dom";
 
-import Users from "./users/users";
-import Places from "./places/places";
+import Users from "./users/pages/Users";
+import Places from "./places/pages/NewPlace";
 
 function App() {
   return (
     <Router>
       {/* <Switch> */}
       <Routes>
-        <Route path="/" element={<Places />} exact />
+        <Route path="/" element={<Users />} exact />
         {/* <Route path="/" exact>
           <Places />
         </Route> */}
         <Route path="/users" element={<Users />} />
-        <Route path="/places" element={<Places />} />
+        <Route path="/places/new" element={<Places />} />
         <Route path="*" element={<Navigate to="/" />} />{" "}
         {/* <Route path="*">
           <redirect to="/" />
