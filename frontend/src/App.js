f53dfcfadc2d32dbb48495572bb4ec33bee0ebe2
Majid.mhdp <Majid.mhdp@gmail.com>
2026-03-18 19:35:53 +0800

    routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+// import { BrowserRouter as Router, Route, Switch, redirect } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
+
+import Users from "./users/users";
+import Places from "./places/places";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      {/* <Switch> */}
+      <Routes>
+        <Route path="/" element={<Places />} exact />
+        {/* <Route path="/" exact>
+          <Places />
+        </Route> */}
+        <Route path="/users" element={<Users />} />
+        <Route path="/places" element={<Places />} />
+        <Route path="*" element={<Navigate to="/" />} />{" "}
+        {/* <Route path="*">
+          <redirect to="/" />
+        </Route> */}
+      </Routes>
+      {/* </Switch> */}
+    </Router>
   );
 }
 
