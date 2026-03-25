56259970c4a46a5ab229da306f82c8aa8ff75647
Majid.mhdp <Majid.mhdp@gmail.com>
2026-03-25 19:10:15 +0800

    react context

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useCallback } from "react";
 // import { BrowserRouter as Router, Route, Switch, redirect } from "react-router-dom";
 import {
   BrowserRouter as Router,
@@ -12,30 +12,67 @@ import UserPlaces from "./places/pages/UserPlaces";
 import Places from "./places/pages/NewPlace";
 import UpdatePlace from "./places/pages/UpdatePlace";
 import MainNavigation from "./shared/components/Navigation/MainNavigation";
+import Auth from "./user/pages/Auth";
+import { AuthContext } from "./shared/context/auth-context";
 
 function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  const login = useCallback(() => {
+    setIsLoggedIn(true);
+  }, []);
+
+  const logout = useCallback(() => {
+    setIsLoggedIn(false);
+  }, []);
+
+  let routes;
+  if (isLoggedIn) {
+    routes = (
+      <React.Fragment>
+        <Route path="/" element={<Users />} exact />
+        <Route path="/users" element={<Users />} />
+        <Route path="/:userId/places" element={<UserPlaces />} />
+        <Route path="/places/new" element={<Places />} />
+        <Route path="/places/:placeId" element={<UpdatePlace />} />
+        <Route path="*" element={<Navigate to="/" />} />
+      </React.Fragment>
+    );
+  } else {
+    routes = (
+      <React.Fragment>
+        <Route path="/" element={<Users />} exact />
+        <Route path="/users" element={<Users />} />
+        <Route path="/:userId/places" element={<UserPlaces />} />
+        <Route path="/auth" element={<Auth />} />
+        <Route path="*" element={<Navigate to="/auth" />} />
+      </React.Fragment>
+    );
+  }
+
   return (
-    <Router>
-      <MainNavigation />
-      <main>
-        {/* <Switch> */}
-        <Routes>
-          <Route path="/" element={<Users />} exact />
-          {/* <Route path="/" exact>
+    <AuthContext.Provider
+      value={{ isLoggedIn: isLoggedIn, login: login, logout: logout }}
+    >
+      <Router>
+        <MainNavigation />
+        <main>
+          {/* <Switch> */}
+          <Routes>
+            {routes}
+            {/* <Route path="/" element={<Users />} exact /> */}
+            {/* <Route path="/" exact>
           <Places />
         </Route> */}
-          <Route path="/users" element={<Users />} />
-          <Route path="/:userId/places" element={<UserPlaces />} />
-          <Route path="/places/new" element={<Places />} />
-          <Route path="/places/:placeId" element={<UpdatePlace />} />
-          <Route path="*" element={<Navigate to="/" />} />{" "}
-          {/* <Route path="*">
+            {/* <Route path="*" element={<Navigate to="/" />} />{" "} */}
+            {/* <Route path="*">
           <redirect to="/" />
         </Route> */}
-        </Routes>
-        {/* </Switch> */}
-      </main>
-    </Router>
+          </Routes>
+          {/* </Switch> */}
+        </main>
+      </Router>
+    </AuthContext.Provider>
   );
 }
 
