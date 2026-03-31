38223c445f47548fcfe513633b86c3062ba44218
Majid.mhdp <Majid.mhdp@gmail.com>
2026-03-31 17:21:01 +0800

    mongodb

--- backend/app.js
+++ backend/app.js
@@ -1,17 +1,18 @@
-const express = require('express');
+const express = require("express");
 // const cors = require('cors');
-const bodyParser = require('body-parser');
+const bodyParser = require("body-parser");
 // const morgan = require('morgan');
 // const { sequelize } = require('./models');
+const mongoose = require("mongoose");
 
-const placesRoutes = require('./routes/places-routes');
-const usersRoutes = require('./routes/users-routes');
-const HttpError = require('./models/http-error');
+const placesRoutes = require("./routes/places-routes");
+const usersRoutes = require("./routes/users-routes");
+const HttpError = require("./models/http-error");
 
 const app = express();
 
-app.use('/api/places', placesRoutes);
-app.use('/api/users', usersRoutes);
+app.use("/api/places", placesRoutes);
+app.use("/api/users", usersRoutes);
 
 // Middleware
 app.use(bodyParser.json());
@@ -19,7 +20,7 @@ app.use(bodyParser.json());
 // app.use(morgan('dev'));
 
 app.use((req, res, next) => {
-  const error = new HttpError('Could not find this route.', 404);
+  const error = new HttpError("Could not find this route.", 404);
   throw error;
 });
 
@@ -27,9 +28,20 @@ app.use((error, req, res, next) => {
   if (res.headerSent) {
     return next(error);
   }
-  res.status(error.code || 500).json({message: error.message || 'An unknown error occurred!'});
+  res
+    .status(error.code || 500)
+    .json({ message: error.message || "An unknown error occurred!" });
 });
 
-app.listen(5000, () => {
-  console.log('Server is running on port 5000');
-});
+mongoose
+  .connect(
+    `mongodb+srv://academind:ORlnOPLKvIH9M9hP@cluster0-ntrwp.mongodb.net/mern?retryWrites=true&w=majority`,
+  )
+  .then(() => {
+    app.listen(5000, () => {
+      console.log("Server is running on port 5000");
+    });
+  })
+  .catch((err) => {
+    console.log(err);
+  });
