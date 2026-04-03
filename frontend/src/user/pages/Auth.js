69a216494ca24d3b5bf62a3a8d230ef2fdbf8c1c
Majid.mhdp <Majid.mhdp@gmail.com>
2026-04-03 19:48:55 +0800

    add place

--- frontend/src/user/pages/Auth.js
+++ frontend/src/user/pages/Auth.js
@@ -65,7 +65,7 @@ const Auth = () => {
 
     if (isLoginMode) {
       try {
-        await sendRequest(
+        const response = await sendRequest(
           "http://localhost:5000/api/users/login",
           "POST",
           JSON.stringify({
@@ -77,13 +77,13 @@ const Auth = () => {
           },
         );
 
-        auth.login();
+        auth.login(response.user.id);
       } catch (err) {
         console.log(err);
       }
     } else {
       try {
-        await sendRequest(
+        const response = await sendRequest(
           "http://localhost:5000/api/users/signup",
           "POST",
           JSON.stringify({
@@ -96,7 +96,7 @@ const Auth = () => {
           },
         );
 
-        auth.login();
+        auth.login(response.user.id);
       } catch (err) {
         console.log(err);
       }
