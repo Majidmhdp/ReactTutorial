dd935aa37f3285e27e90a09b8e10e8736b51f098
Majid.mhdp <Majid.mhdp@gmail.com>
2026-04-05 12:18:14 +0800

    hash pasword

--- backend/controllers/users-controllers.js
+++ backend/controllers/users-controllers.js
@@ -1,28 +1,29 @@
-const { validationResult } = require('express-validator');
+const { validationResult } = require("express-validator");
+const bcrypt = require("bcryptjs");
 
-const HttpError = require('../models/http-error');
-const User = require('../models/user');
+const HttpError = require("../models/http-error");
+const User = require("../models/user");
 
 const getUsers = async (req, res, next) => {
   let users;
   try {
-    users = await User.find({}, '-password');
+    users = await User.find({}, "-password");
   } catch (err) {
     const error = new HttpError(
-      'Fetching users failed, please try again later.',
-      500
+      "Fetching users failed, please try again later.",
+      500,
     );
     return next(error);
   }
-  res.json({ users: users.map(user => user.toObject({ getters: true })) });
+  res.json({ users: users.map((user) => user.toObject({ getters: true })) });
 };
 
 const signup = async (req, res, next) => {
   const errors = validationResult(req);
-  
+
   if (!errors.isEmpty()) {
     return next(
-      new HttpError('Invalid inputs passed, please check your data.', 422)
+      new HttpError("Invalid inputs passed, please check your data.", 422),
     );
   }
   const { name, email, password } = req.body;
@@ -32,16 +33,27 @@ const signup = async (req, res, next) => {
     existingUser = await User.findOne({ email: email });
   } catch (err) {
     const error = new HttpError(
-      'Signing up failed, please try again later.',
-      500
+      "Signing up failed, please try again later.",
+      500,
     );
     return next(error);
   }
 
   if (existingUser) {
     const error = new HttpError(
-      'User exists already, please login instead.',
-      422
+      "User exists already, please login instead.",
+      422,
+    );
+    return next(error);
+  }
+
+  let hashedPassword;
+  try {
+    hashedPassword = await bcrypt.hash(password, 12);
+  } catch (err) {
+    const error = new HttpError(
+      "Could not create user, please try again.",
+      500,
     );
     return next(error);
   }
@@ -50,16 +62,16 @@ const signup = async (req, res, next) => {
     name,
     email,
     image: "http://localhost:5000/" + (req.file ? req.file.path : null),
-    password, 
-    places: []
+    password,
+    places: [],
   });
 
   try {
     await createdUser.save();
   } catch (err) {
     const error = new HttpError(
-      'Signing up failed, please try again later.',
-      500
+      "Signing up failed, please try again later.",
+      500,
     );
     return next(error);
   }
@@ -76,21 +88,43 @@ const login = async (req, res, next) => {
     existingUser = await User.findOne({ email: email });
   } catch (err) {
     const error = new HttpError(
-      'Loggin in failed, please try again later.',
-      500
+      "Loggin in failed, please try again later.",
+      500,
     );
     return next(error);
   }
 
-  if (!existingUser || existingUser.password !== password) {
+  if (!existingUser) {
     const error = new HttpError(
-      'Invalid credentials, could not log you in.',
-      401
+      "Invalid credentials, could not log you in.",
+      401,
     );
     return next(error);
   }
 
-  res.json({ message: 'Logged in!', user: existingUser.toObject({ getters: true }) });
+  let isValidPassword = false;
+  try {
+    isValidPassword = await bcrypt.compare(password, existingUser.password);
+  } catch (err) {
+    const error = new HttpError(
+      "Could not log you in, please check your credentials and try again.",
+      500,
+    );
+    return next(error);
+  }
+
+  if (!isValidPassword) {
+    const error = new HttpError(
+      "Invalid credentials, could not log you in.",
+      401,
+    );
+    return next(error);
+  }
+
+  res.json({
+    message: "Logged in!",
+    user: existingUser.toObject({ getters: true }),
+  });
 };
 
 exports.getUsers = getUsers;
